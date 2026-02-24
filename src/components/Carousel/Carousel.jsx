449310f0e95e2cea3da52a6ea90cd4ad1245074f
sagar gound <sagargound0410@gmail.com>
2026-02-25 03:05:46 +0530

    navigation functionality check

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -3,13 +3,25 @@ import { Navigation } from "swiper/modules";
 import styles from "./Carousel.module.css";
 import "swiper/css";
 import "swiper/css/navigation";
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import LeftNavButton from "../NavButton/LeftNavButton";
 import RightNavButton from "../NavButton/RightNavButton";
 
 function Carousel({ data = [], renderItem }) {
   const prevRef = useRef(null);
   const nextRef = useRef(null);
+  const [swiperInstance, setSwiperInstance] = useState(null);
+
+  useEffect(() => {
+    if (swiperInstance && prevRef.current && nextRef.current) {
+      swiperInstance.params.navigation.prevEl = prevRef.current;
+      swiperInstance.params.navigation.nextEl = nextRef.current;
+
+      swiperInstance.navigation.destroy();
+      swiperInstance.navigation.init();
+      swiperInstance.navigation.update();
+    }
+  }, [swiperInstance]);
 
   return (
     <div className={styles.wrapper}>
@@ -20,26 +32,18 @@ function Carousel({ data = [], renderItem }) {
       <Swiper
         modules={[Navigation]}
         spaceBetween={20}
-        onBeforeInit={(swiper) => {
-          swiper.params.navigation.prevEl = prevRef.current;
-          swiper.params.navigation.nextEl = nextRef.current;
-        }}
+        onSwiper={setSwiperInstance}
         breakpoints={{
           320: { slidesPerView: 1.5 },
           480: { slidesPerView: 3 },
           768: { slidesPerView: 5 },
           1024: { slidesPerView: 7 },
         }}
-        onSwiper={(swiper) => {
-          swiper.params.navigation.prevEl = prevRef.current;
-          swiper.params.navigation.nextEl = nextRef.current;
-
-          swiper.navigation.init();
-          swiper.navigation.update();
-        }}
       >
         {data.map((item) => (
-          <SwiperSlide key={item.id}>{renderItem(item)}</SwiperSlide>
+          <SwiperSlide key={item.id}>
+            {renderItem(item)}
+          </SwiperSlide>
         ))}
       </Swiper>
 
